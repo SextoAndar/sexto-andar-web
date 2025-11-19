7987411115f4990c17b638279a382eb6a9531fed
moonshinerd <vitorhls15@gmail.com>
2025-11-19 09:08:01 -0300

    feat: set up routing with React Router in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import './App.css'
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Adicione novas rotas aqui */}
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
